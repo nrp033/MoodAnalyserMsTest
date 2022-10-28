3e09ee945de445ba4e7c1fb87acb3e8946382446
Nitin Pawar <107263213+nrp033@users.noreply.github.com>
2022-10-28 08:17:54 +0530

    Given I am in Sad Mood Messege will Return SAD

--- MoodAnalyserTest/UnitTest1.cs
+++ MoodAnalyserTest/UnitTest1.cs
@@ -7,11 +7,23 @@ namespace MoodAnalyserTest
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void GivenMood_ShouldReturn_SAD_MOOD()
         {
-           
+            {
+                //Arrange
+                string Message = "I am in Sad Mood";
+                MoodAnalyser MoodCheck = new MoodAnalyser();
 
+                string expected = "SAD";
+
+                //Act
+                string actual = MoodCheck.AnalyseMood(Message);
+
+                //Assert
+                Assert.AreEqual(expected, actual);
+            }
 
         }
+      
     }
 }
