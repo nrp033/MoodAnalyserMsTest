7cb04ffd37d19701c09c4217515dbd6e61b495b6
Nitin Pawar <107263213+nrp033@users.noreply.github.com>
2022-10-28 08:22:37 +0530

    Given I am in Any Mood Messege will Return HAPPY

--- MoodAnalyserTest/UnitTest1.cs
+++ MoodAnalyserTest/UnitTest1.cs
@@ -6,9 +6,9 @@ namespace MoodAnalyserTest
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
-        public void GivenMood_ShouldReturn_SAD_MOOD()
-        {
+            [TestMethod]
+            public void GivenMood_ShouldReturn_SAD_MOOD()
+        
             {
                 //Arrange
                 string Message = "I am in Sad Mood";
@@ -23,7 +23,23 @@ namespace MoodAnalyserTest
                 Assert.AreEqual(expected, actual);
             }
 
-        }
-      
+
+            [TestMethod]
+            public void GivenAnyMood_ShouldReturn_Happy()
+            {
+                //Arrange
+                string Message = "I am In Any Mood";
+                MoodAnalyser MoodCheck = new MoodAnalyser();
+                string expected = "HAPPY";
+
+                //Act
+                string actual = MoodCheck.AnalyseMood(Message);
+
+                //Assert
+                Assert.AreEqual(expected, actual);
+            }
+
     }
+      
+    
 }
